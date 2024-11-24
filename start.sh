c8ae03d7131cd295d33548c1489f19f57d288c61
Jose Daniel Hernandez Cano <j.hernandez@cyclopslab.com>
2024-11-24 16:29:04 -0500

    Eliminacion de start.sh afuera de src

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Ejecutar los archivos de entrenamiento
-echo "Entrenando Modelo Scoring"
-python scoring.py
-
-# Iniciar la aplicación FastAPI
-echo "Iniciando la API..."
-uvicorn app:app --host 0.0.0.0 --port 8000
