d2f76e3b39d3c3cf3e267fe89f3c5a51dbd440d4
Jose Daniel Hernandez Cano <j.hernandez@cyclopslab.com>
2025-07-01 16:49:58 -0500

    feat: make most scoring model fields optional
    Modified the SolicitudScoring model to make all fields optional except for:
    - ingresos
    - gastos
    - activos
    - pasivos
    - monto_solicitado
    - destino_credito

--- src/models/predict_data.py
+++ src/models/predict_data.py
@@ -3,23 +3,23 @@ from typing import Literal, List, Optional
 
 # Definición del modelo de datos de entrada para la predicción del scoring
 class SolicitudScoring(BaseModel):
-    edad: conint(ge=19, le=100)  # Edad entre 18 y 100
-    estado_civil: Literal['Soltero', 'Divorciado', 'Viudo', 'Casado', 'Unión Libre']  # Se limita a estos valores
-    tipo_de_vivienda: Literal['Arrendada', 'Familiar', 'Propia']  # Se limita a estos valores
-    estrato: conint(ge=1, le=6)  # Estrato socioeconómico
-    nivel_academico: Literal['Educación básica', 'Bachiller', 'Pregrado', 'Posgrado']  # Nivel académico
-    profesion : Literal["2145"]  # Tipo de ocupación (texto libre)
-    tipo_contrato: Literal['Contrato temporal', 'Contrato por obra o labor', "Contrato de aprendizaje", "Contrato de trabajo a término fijo","Contrato de trabajo a término indefinido"]  # Tipo de contrato
-    antiguedad_trabajo: conint(ge=0)  # Antigüedad laboral en años
-    cargo: Literal["Técnico","Asistencial","Profesional", "Asesor", "Directivo"]  # Cargo (texto libre)
+    edad: Optional[conint(ge=19, le=100)] = None  # Edad entre 18 y 100
+    estado_civil: Optional[Literal['Soltero', 'Divorciado', 'Viudo', 'Casado', 'Unión Libre']] = None  # Se limita a estos valores
+    tipo_de_vivienda: Optional[Literal['Arrendada', 'Familiar', 'Propia']] = None  # Se limita a estos valores
+    estrato: Optional[conint(ge=1, le=6)] = None  # Estrato socioeconómico
+    nivel_academico: Optional[Literal['Educación básica', 'Bachiller', 'Pregrado', 'Posgrado']] = None   # Nivel académico
+    profesion : Optional[Literal["2145"]] = None  # Tipo de ocupación (texto libre)
+    tipo_contrato: Optional[Literal['Contrato temporal', 'Contrato por obra o labor', "Contrato de aprendizaje", "Contrato de trabajo a término fijo","Contrato de trabajo a término indefinido"]] = None  # Tipo de contrato
+    antiguedad_trabajo: Optional[conint(ge=0)] = None  # Antigüedad laboral en años
+    cargo: Optional[Literal["Técnico","Asistencial","Profesional", "Asesor", "Directivo"]] = None  # Cargo (texto libre)
     ingresos: condecimal(ge=0)  # Ingresos totales
-    gastos: condecimal(ge=0)  # Gastos totales
-    activos: condecimal(ge=0)  # Activos totales
+    gastos: condecimal(ge=0)   # Gastos totales
+    activos: condecimal(ge=0)   # Activos totales
     pasivos: condecimal(ge=0)  # Pasivos totales
-    origen_fondos: Literal["Salario"] 
-    declara_renta: Literal['Sí', 'No']  # Declara renta (Sí/No)
-    es_pep: Literal['No']  # Es persona políticamente expuesta (Sí/No)
-    responsabilidad_fiscal_exterior: Literal['No']  # Responsabilidad fiscal en el exterior (Sí/No)
-    tipo_credito: Literal['Crédito de consumo']  # Tipo de crédito
-    monto_solicitado: condecimal(ge=0)  # Monto solicitado
-    destino_credito: Literal['Viajes y turismo', 'viajes', 'Viajes']  # Destino del crédito
+    origen_fondos: Optional[Literal["Salario"]] = None
+    declara_renta: Optional[Literal['Sí', 'No']] = None # Declara renta (Sí/No)
+    es_pep: Optional[Literal['No']] = None # Es persona políticamente expuesta (Sí/No)
+    responsabilidad_fiscal_exterior: Optional[Literal['No']] = None # Responsabilidad fiscal en el exterior (Sí/No)
+    tipo_credito: Optional[Literal['Crédito de consumo']] = None # Tipo de crédito
+    monto_solicitado: condecimal(ge=0) # Monto solicitado
+    destino_credito: Literal['Viajes y turismo', 'viajes', 'Viajes'] # Destino del crédito
